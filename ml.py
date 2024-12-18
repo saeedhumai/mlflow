a6229ee99ea5f5e1c1a85ef9b717caaf2a9b2ece
saeedmaktekai <get.maktek@gmail.com>
2024-12-18 23:37:10 +0400

    mlflow setup

---
+++ ml.py
@@ -0,0 +1,28 @@
+# Set OpenAI API Key to the environment variable. You can also pass the token to dspy.LM()
+import getpass
+import os
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get OpenAI API key from environment variable
+openai_api_key = os.getenv("OPENAI_API_KEY")
+import dspy
+
+# Define your model. We will use OpenAI for simplicity
+model_name = "gpt-4o-mini"
+
+# Leverage default authentication inside the Databricks context (notebooks, workflows, etc.)
+# Note that an OPENAI_API_KEY environment must be present. You can also pass the token to dspy.LM()
+lm = dspy.LM(
+    model=f"openai/{model_name}",
+    max_tokens=500,
+    temperature=0.1,
+)
+dspy.settings.configure(lm=lm)
+
+
+
+
